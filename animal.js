111fa078701b80f1ce700f003e9de7c428200eab
liarakowalis-ship-it <liara.kowalis@gmail.com>
2026-09-17 14:58:50 +0200

    Update animal.js

--- animal.js
+++ animal.js
@@ -1,6 +1,29 @@
 function showAnimal(animalId) {
-    alert("It works! You clicked on " + animalId);
+    const overview = document.getElementById("animal-overview");
+    const information = document.getElementById("animal-information");
+
+    overview.style.display = "none";
+    information.style.display = "block";
+
+    const animal = animals[animalId];
+
+    document.getElementById("animal-name").textContent = animal.name;
+    document.getElementById("animal-image").src = animal.image;
+    document.getElementById("animal-image").alt = animal.name;
+    document.getElementById("habitat").textContent = animal.habitat;
+
+    document.getElementById("look").innerHTML =
+        animal.look.map(item => "<li>" + item + "</li>").join("");
+
+    document.getElementById("characteristics").innerHTML =
+        animal.characteristics.map(item => "<li>" + item + "</li>").join("");
+
+    document.getElementById("general").innerHTML =
+        animal.general.map(item => "<li>" + item + "</li>").join("");
+
+    document.getElementById("funfact").textContent = animal.funfact;
 }
+
 
 function backToAnimals() {
     document.getElementById("animal-information").style.display = "none";
